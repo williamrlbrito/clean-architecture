b41c5ad2d397053c6127ec12c6e91fc1a9b43fcb
William Brito <williamrldb@gmail.com>
2023-06-11 14:21:58 -0400

    --Retornando erro em teste de unidade

--- src/usecase/customer/find/find.customer.usecase.unit.spec.ts
+++ src/usecase/customer/find/find.customer.usecase.unit.spec.ts
@@ -38,4 +38,16 @@ describe("Unit Test find customer use case", () => {
 
     expect(result).toEqual(output);
   });
+
+  it("should not found a customer", async () => {
+    const customerRepository = MockRepository();
+    customerRepository.find.mockImplementation(() => {
+      throw new Error("Customer not found");
+    });
+    const usecase = new FindCustomerUseCase(customerRepository);
+
+    const input = { id: customer.id };
+
+    await expect(usecase.execute(input)).rejects.toThrow("Customer not found");
+  });
 });
