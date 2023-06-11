78a332c436bb849af698726eab8d95f43d6527a5
William Brito <williamrldb@gmail.com>
2023-06-11 16:32:18 -0400

    --Criando UseCase para alterar Customer

---
+++ src/usecase/customer/update/update.customer.dto.ts
@@ -0,0 +1,23 @@
+interface InputUpdateCustomerDto {
+  id: string;
+  name: string;
+  address: {
+    street: string;
+    number: number;
+    zip: string;
+    city: string;
+  };
+}
+
+interface OutputUpdateCustomerDto {
+  id: string;
+  name: string;
+  address: {
+    street: string;
+    number: number;
+    zip: string;
+    city: string;
+  };
+}
+
+export { InputUpdateCustomerDto, OutputUpdateCustomerDto };
