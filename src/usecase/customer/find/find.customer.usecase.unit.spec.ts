c3a19b891c51b18e87af70ee8b24086637b722f8
William Brito <williamrldb@gmail.com>
2023-06-11 13:59:26 -0400

    --Criando primeiro teste de Unidade

---
+++ src/usecase/customer/find/find.customer.usecase.unit.spec.ts
@@ -0,0 +1,41 @@
+import CustomerFactory from "../../../domain/customer/factory/customer.factory";
+import Address from "../../../domain/customer/value-object/address";
+import FindCustomerUseCase from "./find.customer.usecase";
+
+const customer = CustomerFactory.createWithAddress(
+  "John Doe",
+  new Address("Rua A", 123, "12345-123", "São Paulo")
+);
+
+const MockRepository = () => {
+  return {
+    find: jest.fn().mockReturnValue(Promise.resolve(customer)),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit Test find customer use case", () => {
+  it("should find a customer", async () => {
+    const customerRepository = MockRepository();
+    const usecase = new FindCustomerUseCase(customerRepository);
+
+    const input = { id: customer.id };
+
+    const output = {
+      id: customer.id,
+      name: customer.name,
+      address: {
+        street: customer.Address.street,
+        city: customer.Address.city,
+        number: customer.Address.number,
+        zip: customer.Address.zip,
+      },
+    };
+
+    const result = await usecase.execute(input);
+
+    expect(result).toEqual(output);
+  });
+});
