76e9acf90918f568577958d32d3a2698466aabcd
William Brito <williamrldb@gmail.com>
2023-06-12 17:54:15 -0400

    --Testando usecase para listagem de Customer

---
+++ src/usecase/customer/list/list.customer.unit.spec.ts
@@ -0,0 +1,40 @@
+import CustomerFactory from "../../../domain/customer/factory/customer.factory";
+import Address from "../../../domain/customer/value-object/address";
+
+const customerOne = CustomerFactory.createWithAddress(
+  "John Doe",
+  new Address("John street", 2023, "John zip", "John city")
+);
+
+const customerTwo = CustomerFactory.createWithAddress(
+  "Jane Doe",
+  new Address("Jane street", 2023, "Jane zip", "Jane city")
+);
+
+const MockRepository = () => {
+  return {
+    find: jest.fn(),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest
+      .fn()
+      .mockReturnValue(Promise.resolve([customerOne, customerTwo])),
+  };
+};
+
+describe("Unit Test for listing customer use case", () => {
+  it("should list all customers", async () => {
+    const repository = MockRepository();
+    const usecase = new ListCustomerUseCase(repository);
+
+    const output = await usecase.execute({});
+
+    expect(output.customers).toHaveLength(2);
+    expect(output.customers[0].id).toBe(customerOne.id);
+    expect(output.customers[0].name).toBe(customerOne.name);
+    expect(output.customers[0].address.street).toBe(customerOne.Address.street);
+    expect(output.customers[1].id).toBe(customerTwo.id);
+    expect(output.customers[1].name).toBe(customerTwo.name);
+    expect(output.customers[1].address.street).toBe(customerTwo.Address.street);
+  });
+});
