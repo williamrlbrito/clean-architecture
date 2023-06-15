bea1b6425916cb05dde9543acacc5ce679064867
William Brito <williamrldb@gmail.com>
2023-06-15 10:27:53 -0400

    --Criando primeiro endpoint

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
 import { Sequelize } from "sequelize-typescript";
 import CustomerModel from "../customer/repository/sequelize/customer.model";
+import { customerRoute } from "./routes/customer.route";
 
 const app: Express = express();
 app.use(express.json());
+app.use("/customers", customerRoute);
 
 let sequelize: Sequelize;
 
