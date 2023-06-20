a0cf32b8258e2355e517a88f84457f9be53d5096
William Brito <williamrldb@gmail.com>
2023-06-20 10:30:32 -0400

    --Implementando o Notification

---
+++ src/domain/@shared/notification/notification.ts
@@ -0,0 +1,27 @@
+type NotificationError = {
+  message: string;
+  context: string;
+};
+
+class Notification {
+  private errors: NotificationError[] = [];
+
+  addError(error: NotificationError) {
+    this.errors.push(error);
+  }
+
+  messages(context?: string): string {
+    if (context) {
+      return this.errors
+        .filter((error) => error.context === context)
+        .map((error) => `${error.context}: ${error.message}`)
+        .join(", ");
+    }
+
+    return this.errors
+      .map((error) => `${error.context}: ${error.message}`)
+      .join(", ");
+  }
+}
+
+export { Notification, NotificationError };
