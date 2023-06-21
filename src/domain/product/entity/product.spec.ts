ad92de1d6b4a0529e44d7381882d677337c0c041
William Brito <williamrldb@gmail.com>
2023-06-21 09:34:58 -0400

    --Implemented Notification Standard on Products

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -3,20 +3,26 @@ import Product from "./product";
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
-      const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+      let product = new Product("", "Product 1", 100);
+    }).toThrowError("product: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
       const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+    }).toThrowError("product: Name is required");
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+    }).toThrowError("product: Price must be greater than zero");
+  });
+
+  it("should throw error when id and name are empty", () => {
+    expect(() => {
+      const product = new Product("", "", 100);
+    }).toThrowError("product: Id is required, product: Name is required");
   });
 
   it("should change name", () => {
